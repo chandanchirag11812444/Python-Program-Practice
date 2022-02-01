a689c75e30f84e3cdfa393737da5d2f323880ff0
Chandan Chirag <93903887+chandanchirag11812444@users.noreply.github.com>
2022-02-01 11:23:58 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,7 @@
+#Author:Chandan Chirag
+#licence name:ABC company
+'''Author:Chandan Chirag
+#licence name:ABC company'''#triple single for multiple line comments
+import os #importing the os module 
+
+print("hello world")
